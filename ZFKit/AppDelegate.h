30f9d8346a66cf0a4beb8920099403cce15114ce
刘占峰 <18272734568@163.com>
2017-12-12 09:56:11 +0800

    init

---
+++ ZFKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZFKit
+//
+//  Created by Daisy on 2017/12/12.
+//  Copyright © 2017年 zf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZFKit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZFKit
+//
+//  Created by Daisy on 2017/12/12.
+//  Copyright © 2017年 zf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
